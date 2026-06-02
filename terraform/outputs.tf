ce5485351a3f5bc7a5ec841eb2a851cb06cc1d62
edoturb <eduardourbin@gmail.com>
2026-06-01 20:57:51 -0400

    fix: actualiza outputs.tf para usar outputs de modulos

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,14 @@
-# Salidas publicas de la raiz del proyecto
 output "instancia_ip_publica" {
-  value       = aws_instance.vm.public_ip
+  value       = module.compute.instance_ip
   description = "La direccion IP publica de la maquina EC2"
+}
+
+output "vpc_id" {
+  value       = module.network.vpc_id
+  description = "ID de la VPC creada"
+}
+
+output "bucket_name" {
+  value       = module.storage.bucket_name
+  description = "Nombre del bucket S3 creado"
 }
